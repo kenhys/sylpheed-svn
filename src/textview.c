41b2cdf41ac8b16a4e703959b6221c2ad073a63c
Kentaro Hayashi <kenhys@gmail.com>
2018-10-17 22:30:04 +0900

    textview: use gtk_widget_get_style (#123)
    Signed-off-by: Kentaro Hayashi <kenhys@gmail.com>

--- src/textview.c
+++ src/textview.c
@@ -767,7 +767,7 @@ static gboolean textview_part_widget_exposed(GtkWidget *widget,
 		return FALSE;
 
 	drawable = GDK_DRAWABLE(gtk_widget_get_window(widget));
-	gc = widget->style->dark_gc[gtkut_widget_get_state(widget)];
+	gc = gtk_widget_get_style(widget)->dark_gc[gtkut_widget_get_state(widget)];
 	gdk_gc_set_clip_rectangle(gc, &event->area);
 	gdk_gc_set_line_attributes(gc, 1, GDK_LINE_SOLID, GDK_CAP_NOT_LAST,
 				   GDK_JOIN_MITER);
